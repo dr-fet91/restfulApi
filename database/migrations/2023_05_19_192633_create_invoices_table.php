722e4fa42a05905f2e425f98645dda34a23a1087
dr-fet91 <dr.fet91@gmail.com>
2023-05-19 23:15:47 +0330

    Designing & Seeding the Database

---
+++ database/migrations/2023_05_19_192633_create_invoices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->nullable()->constrained('customers')->nullOnDelete()->cascadeOnUpdate();
+            $table->integer('amount');
+            $table->enum('status', ['Billed', 'Paid', 'Void']);
+            $table->dateTime('billed_date');
+            $table->datetime('paid_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
